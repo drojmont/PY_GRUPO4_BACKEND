bb524a3f98e44f7488ec3b8cea68e596dcf397dd
Mayda <mimayjo@gmail.com>
2025-02-16 20:41:19 -0500

    May_agregado cambio de lista de rect almacenar string DB

--- src/main/java/com/backend/tourBooking/dto/input/ProductInputDTO.java
+++ src/main/java/com/backend/tourBooking/dto/input/ProductInputDTO.java
@@ -11,6 +11,31 @@ import java.util.List;
 @Setter
 public class ProductInputDTO {
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(List<String> imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
     private String description;
 
     //@NotEmpty(message = "La lista de imágenes no puede estar vacía")
--- src/main/java/com/backend/tourBooking/entity/Product.java
+++ src/main/java/com/backend/tourBooking/entity/Product.java
@@ -4,10 +4,11 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-@Getter
-@Setter
+
 @Entity
 @Table(name = "PRODUCT")
 public class Product {
@@ -16,13 +17,53 @@ public class Product {
     private Long id;
     private String name;
     private String description;
-    private List<String> imageUrl;
+    private String imageUrl;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
 
     public Product() {}
 
-    public Product(String name, String description, List<String> imageUrl) {
+    public Product(String name, String description, String imageUrl) {
         this.name = name;
         this.description = description;
+        this.imageUrl =imageUrl;
+    }
+
+    public List<String> getStringUrlAsList(){
+        return this.imageUrl != null ? new ArrayList<>(Arrays.asList(this.imageUrl.split(","))) : new ArrayList<>();
+
+    }
+    public void setImageUrlFromList(List<String> imageUrlList) {
+        this.imageUrl = (imageUrlList == null || imageUrlList.isEmpty()) ? "" : String.join(",", imageUrlList);
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setImageUrl(String imageUrl) {
         this.imageUrl = imageUrl;
     }
 }
