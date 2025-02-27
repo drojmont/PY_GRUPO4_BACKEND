db432ead682de7f03281c22886cf8d1d0952e9e1
Mayda <mimayjo@gmail.com>
2025-02-27 18:01:14 -0500

    Guardando cambios en MayCategoria

--- src/main/java/com/backend/tourBooking/service/ICategoryService.java
+++ src/main/java/com/backend/tourBooking/service/ICategoryService.java
@@ -1,6 +1,8 @@
 package com.backend.tourBooking.service;
 
+import com.backend.tourBooking.dto.input.ProductInputDTO;
 import com.backend.tourBooking.dto.output.CategoryOutputDTO;
+import com.backend.tourBooking.dto.output.ProductOutputDTO;
 import com.backend.tourBooking.entity.Category;
 
 import java.util.List;
@@ -8,4 +10,5 @@ import java.util.List;
 public interface ICategoryService {
     Category getCategoryById(Long id_categoria);
     List<CategoryOutputDTO> getAllCategories();
+    CategoryOutputDTO saveCategory(ProductInputDTO productInputDTO);
 }
--- src/main/java/com/backend/tourBooking/service/impl/CategoryService.java
+++ src/main/java/com/backend/tourBooking/service/impl/CategoryService.java
@@ -1,6 +1,7 @@
 package com.backend.tourBooking.service.impl;
 
 
+import com.backend.tourBooking.dto.input.ProductInputDTO;
 import com.backend.tourBooking.dto.output.CategoryOutputDTO;
 import com.backend.tourBooking.entity.Category;
 import com.backend.tourBooking.repository.CategoryRepository;
@@ -34,6 +35,11 @@ public class CategoryService implements ICategoryService {
     public List<CategoryOutputDTO> getAllCategories() {
         List<CategoryOutputDTO> categoryOutputDTOS = categoryRepository.findAll().stream().map(category -> modelMapper.map(category, CategoryOutputDTO.class)).toList();
         return categoryOutputDTOS;
+    }
+
+    @Override
+    public CategoryOutputDTO saveCategory(ProductInputDTO productInputDTO) {
+        return null;
     }
 
 }
