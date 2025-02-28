c8592cc4b3eee532ec020b6bbf3328b175384caf
Mayda <mimayjo@gmail.com>
2025-02-27 20:15:17 -0500

    solucionando el cotroller

--- src/main/java/com/backend/tourBooking/service/ICategoryService.java
+++ src/main/java/com/backend/tourBooking/service/ICategoryService.java
@@ -1,5 +1,6 @@
 package com.backend.tourBooking.service;
 
+import com.backend.tourBooking.dto.input.CategoryInputDTO;
 import com.backend.tourBooking.dto.input.ProductInputDTO;
 import com.backend.tourBooking.dto.output.CategoryOutputDTO;
 import com.backend.tourBooking.dto.output.ProductOutputDTO;
@@ -8,6 +9,7 @@ import com.backend.tourBooking.entity.Category;
 import java.util.List;
 
 public interface ICategoryService {
+    CategoryOutputDTO saveCategory(CategoryInputDTO categorytInputDTO);
     Category getCategoryById(Long id_categoria);
     List<CategoryOutputDTO> getAllCategories();
     CategoryOutputDTO saveCategory(ProductInputDTO productInputDTO);
