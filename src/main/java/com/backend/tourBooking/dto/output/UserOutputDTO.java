2d6bb2841636404c3a3f445c5809d6165cf8107b
drojmont <dylan.rojas@correounivale.edu.co>
2025-03-02 12:08:11 -0500

    DylanRM-encapsulamiento de UserInputDTO y UserOutputDto configuracion Base de datos en EC2

--- src/main/java/com/backend/tourBooking/dto/output/UserOutputDTO.java
+++ src/main/java/com/backend/tourBooking/dto/output/UserOutputDTO.java
@@ -12,5 +12,46 @@ public class UserOutputDTO {
     private String nombre;
     private String apellido;
     private String correoElectronico;
-    
+
+    public UserOutputDTO() {
+    }
+
+    public UserOutputDTO(Long id, String nombre, String apellido, String correoElectronico) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correoElectronico = correoElectronico;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
 }
--- src/main/java/com/backend/tourBooking/entity/User.java
+++ src/main/java/com/backend/tourBooking/entity/User.java
@@ -29,4 +29,64 @@ public class User {
     private String password;
     
     private boolean activo = true;
+
+    public User() {
+    }
+
+    public User(Long id, String nombre, String apellido, String correoElectronico, String password, boolean activo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correoElectronico = correoElectronico;
+        this.password = password;
+        this.activo = activo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
 }
