44313188f39688cee4c472938c0d3bfd24996083
Dylan Rojas M <99608919+drojmont@users.noreply.github.com>
2025-03-03 18:17:00 -0500

    Revert "Agrego historia de usuario 19"

--- src/main/java/com/backend/tourBooking/TourBookingApplication.java
+++ src/main/java/com/backend/tourBooking/TourBookingApplication.java
@@ -3,9 +3,7 @@ package com.backend.tourBooking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
-
 public class TourBookingApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/backend/tourBooking/service/IMailService.java
+++
@@ -1,8 +0,0 @@
-package com.backend.tourBooking.service;
-
-import com.backend.tourBooking.dto.MailDTO;
-import jakarta.mail.MessagingException;
-
-public interface IMailService {
-    public void sendMail(MailDTO mail) throws MessagingException;
-}
